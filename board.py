5c5bd3ecc8e75b87ba3b43ef679aa2f89f217c15
Saurav <38475004+SauravC99@users.noreply.github.com>
2021-05-15 02:14:03 -0700

    Implementation

---
+++ board.py
@@ -0,0 +1,64 @@
+import random
+import numpy as np
+
+
+class Board:
+    def __init__(self, n):
+        self.n_queen = n
+        self.map = [[0 for j in range(n)] for i in range(n)]
+        self.fit = 0
+
+        for i in range(self.n_queen):
+            j = random.randint(0, self.n_queen - 1)
+            self.map[i][j] = 1
+
+    def fitness(self):
+        self.fit = 0
+        for i in range(self.n_queen):
+            for j in range(self.n_queen):
+                if self.map[i][j] == 1:
+                    for k in range(1, self.n_queen - i):
+                        if self.map[i + k][j] == 1:
+                            self.fit += 1
+                        if j - k >= 0 and self.map[i + k][j - k] == 1:
+                            self.fit += 1
+                        if j + k < self.n_queen and self.map[i + k][j + k] == 1:
+                            self.fit += 1
+
+    def show(self):
+        print(np.matrix(self.map))
+        print("Fitness: ",  self.fit)
+
+    def flip(self, i, j):
+        if self.map[i][j] == 0:
+            self.map[i][j] = 1
+        else:
+            self.map[i][j] = 0
+
+    def get_map(self):
+        return self.map
+    
+    def get_fit(self):
+        return self.fit
+
+    #Added helper functions
+
+    def getStringRep(self):
+        result = ""
+        for i in range(self.n_queen):
+          for j in range(self.n_queen):
+            if self.map[i][j] == 1:
+              result += str(j + 1)
+        return result
+
+    def setBoardfromString(self, s):
+        self.map = [[0 for j in range(self.n_queen)] for i in range(self.n_queen)]
+        for i in range(self.n_queen):
+          num = int(s[i:i+1])
+          self.map[i][num - 1] = 1
+        
+            
+if __name__ == '__main__':
+    test = Board(5)
+    test.fitness()
+    test.show()    
